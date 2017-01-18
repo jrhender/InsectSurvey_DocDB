ccb4a5ea6e01de0ed8c936ca5e094dee5e3c1bfa
John Henderson <johnhenderson45@gmail.com>
2017-01-17 22:18:55 -0800

    Creation of a few initial files

---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+ public class Item
+ {
+     [JsonProperty(PropertyName = "id")]
+     public string Id { get; set; }
+
+     [JsonProperty(PropertyName = "name")]
+     public string Name { get; set; }
+
+     [JsonProperty(PropertyName = "description")]
+     public string Description { get; set; }
+
+     [JsonProperty(PropertyName = "isComplete")]
+     public bool Completed { get; set; }
+ }
