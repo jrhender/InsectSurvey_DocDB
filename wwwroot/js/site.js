abce849277acae728c3e6cf17f0a46571a9feb0d
John Henderson <johnhenderson45@gmail.com>
2017-01-24 09:01:09 -0800

    Submit using labels is working

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,20 +4,41 @@ $(document).ready(function() {
     var errorFunction = function(jqXHR, textStatus, errorThrown){
         jqXHR.responseText;
     }
+
+    $('.error').hide();
     
     //var createRecordButton = $("#createRecord");
     var createRecordButton = document.getElementById("createRecord");
     createRecordButton.onclick = function(){
         //JSON data
         var dataType = 'application/x-www-form-urlencoded; charset=utf-8';
+        
+        var tableID = $("input#tableID").val();
+  		if (tableID == "") {
+        $("label#tableID_error").show();
+        $("input#tableID").focus();
+        return false;
+        }
+
+  		var score = $("input#score").val();
+  		if (score == "") {
+        $("label#score_error").show();
+        $("input#score").focus();
+        return false;
+        }
+
+  		var comments = $("input#comments").val();
+  		if (comments == "") {
+        $("label#comments_error").show();
+        $("input#comments").focus();
+        return false;
+        }
+
         var data = {
             QuestionID: 1,
-            TableID: 10,
-            Score: 5,
-            Comments: "Great question!"
-            // FirstName: 'Andrew',
-            // LastName: 'Lock',
-            // Age: 31
+            TableID: tableID,
+            Score: score,
+            Comments: comments
         };
 
         console.log('Submitting form...');
